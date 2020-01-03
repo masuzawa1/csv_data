72908e0fe056d1a9cff5c9f1c2af797ad6e33cca
masuzawa1 <57562777+masuzawa1@users.noreply.github.com>
2020-01-03 18:36:35 +0900

    add import function to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  def self.import(path)
+    CSV.foreach(path, headers: true) do |row|
+      User.create(
+        name: row["name"],
+        age: row["age"],
+        address: row["address"]
+      )
+    end
+  end
 end
